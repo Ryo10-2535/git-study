f04c87ddf5ab90b4a232d0884af2db9ca0c819e7
Nakai <n.ryoto9809@gmail.com>
2026-03-18 17:12:47 +0900

    4回目のコミット：インデント崩れの修正と空行の削除

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 // Main.java
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+    	System.out.println("Hello Git!");
     	System.out.println("2回目の修正です");
     	System.out.println("3回目の修正です_0318");
-    	
     }
 }
