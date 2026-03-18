ad0f0b023769cb423a2dbcaab36038f408cb1eb2
Nakai <n.ryoto9809@gmail.com>
2026-03-18 16:53:24 +0900

    3回目のコミット_0318

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
     	System.out.println("2回目の修正です");
+    	System.out.println("3回目の修正です_0318");
+    	
     }
 }
