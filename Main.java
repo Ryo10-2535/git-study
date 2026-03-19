3365cf1d0a47f66519d16660758c3d10ded803f1
Nakai <n.ryoto9809@gmail.com>
2026-03-19 11:52:35 +0900

    5回目のコミット：メッセージ（4つ目）を追加

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
     	System.out.println("Hello Git!");
     	System.out.println("2回目の修正です");
     	System.out.println("3回目の修正です_0318");
+    	System.out.println("5回目の修正です_0319");
     }
 }
